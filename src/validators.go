2394d34ca56a0843e9c253999733ad031dd6226e
exception-raised <dimmelan@riseup.net>
2025-02-22 11:26:59 +0200

    feat: add IsValidPassword function to the validator package

--- src/validators.go
+++ src/validators.go
@@ -3,3 +3,7 @@ package validator
 func IsValidEmail(email string) bool {
 	return isValidEmail(email)
 }
+
+func IsValidPassword(password string, minLength int, maxLength int, regex string) bool {
+	return isValidPassword(password, minLength, maxLength, regex)
+}
