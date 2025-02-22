5b57898eff2097589aaaf74f80f52073c8e5a5b6
exception-raised <dimmelan@riseup.net>
2025-02-22 11:27:10 +0200

    test: add unit tests for IsValidPassword function in the validator package

--- src/validators_test.go
+++ src/validators_test.go
@@ -36,3 +36,67 @@ func TestIsValidEmail(t *testing.T) {
 		})
 	}
 }
+
+func TestIsPasswordValid(t *testing.T) {
+	type args struct {
+		password  string
+		minLength int
+		maxLength int
+		regex     string
+	}
+
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			name: "valid password",
+			args: args{
+				password:  "testtest",
+				minLength: 8,
+				maxLength: 16,
+				regex:     "",
+			},
+			want: true,
+		},
+		{
+			name: "invalid password",
+			args: args{
+				password:  "",
+				minLength: 8,
+				maxLength: 16,
+				regex:     "",
+			},
+			want: false,
+		},
+		{
+			name: "valid password with symbols and digits",
+			args: args{
+				password:  "Test123!",
+				minLength: 8,
+				maxLength: 16,
+				regex:     validateStrictPasswordPattern,
+			},
+			want: true,
+		},
+		{
+			name: "invalid password with symbols and digits",
+			args: args{
+				password:  "test123",
+				minLength: 8,
+				maxLength: 16,
+				regex:     validateStrictPasswordPattern,
+			},
+			want: false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := IsValidPassword(tt.args.password, tt.args.minLength, tt.args.maxLength, tt.args.regex); got != tt.want {
+				t.Errorf("IsValidPassword() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
